685eded43dc827d9698f8e52fc619886252e90fb
Miryoku <dragomore2804@gmail.com>
2024-02-22 20:22:13 +0200

    Refactor logic to sort method

--- src/ItemCard.java
+++ src/ItemCard.java
@@ -1,3 +1,6 @@
+import java.util.Arrays;
+import java.util.Comparator;
+
 public class ItemCard {
     public String title;
     public String description;
@@ -10,9 +13,7 @@ public class ItemCard {
         this.price = price;
         this.discount = discount;
     }
-    public String getTitle(){
-        return this.title;
-    }
+    public String getTitle(){ return this.title; }
     public String getDescription(){
         return this.description;
     }
@@ -21,5 +22,62 @@ public class ItemCard {
     }
     public double getDiscount() {
         return discount;
+    }
+
+    public static void getByField( ItemCard[] items, String sortBy ) {
+        Comparator<ItemCard> comparator;
+        switch( sortBy.toLowerCase() ){
+            case "title" : {
+                comparator = Comparator.comparing( item -> item.title );
+                break;
+            }
+            case "description" : {
+                comparator = Comparator.comparing( item -> item.description );
+                break;
+            }
+            case "price" : {
+                comparator = Comparator.comparingDouble( item -> item.price );
+                break;
+            }
+            case "discount" : {
+                comparator = Comparator.comparingDouble( item -> item.discount );
+                break;
+            }
+            default :
+                throw new IllegalArgumentException("Invalid field to sort");
+        }
+        Arrays.sort( items, comparator );
+    }
+
+    public static void getByField( ItemCard[] items, String sortBy, String sortOrder ) {
+        Comparator<ItemCard> comparator;
+        switch( sortBy.toLowerCase() ){
+            case "title" : {
+                comparator = Comparator.comparing( item -> item.title );
+                break;
+            }
+            case "description" : {
+                comparator = Comparator.comparing( item -> item.description );
+                break;
+            }
+            case "price" : {
+                comparator = Comparator.comparingDouble( item -> item.price );
+                break;
+            }
+            case "discount" : {
+                comparator = Comparator.comparingDouble( item -> item.discount );
+                break;
+            }
+            default :
+                throw new IllegalArgumentException("Invalid field to sort");
+        }
+        if ( sortOrder.equalsIgnoreCase("asc") ) {
+            Arrays.sort( items, comparator );
+        }
+        else if( sortOrder.equalsIgnoreCase( "desc" ) ) {
+            Arrays.sort( items, comparator.reversed() );
+        }
+        else
+            throw new IllegalArgumentException("Invalid sort orded");
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,79 +1,14 @@
-import java.util.ArrayList;
-import java.util.Collections;
-
 public class Main {
     public static void main(String[] args) {
         ItemCard ItemCard1 = new ItemCard("Руль", "Logitech", 5435, 0);
         ItemCard ItemCard2 = new ItemCard("КПП", "Коробка передач для руля", 1534, 10);
         ItemCard ItemCard3 = new ItemCard("Педали", "Педали для руля", 546, 0);
 
-        ArrayList<ItemCard> cards = new ArrayList<>();
-        cards.add(ItemCard1);
-        cards.add(ItemCard2);
-        cards.add(ItemCard3);
+        ItemCard[] items = { ItemCard1, ItemCard2, ItemCard3 };
+        ItemCard.getByField( items, "price");
 
-//        ArrayList<String> sort = getByField(cards, "title");
-//        ArrayList<String> sort = getByField(cards, "description");
-        ArrayList<String> sort = getByField(cards, "price", "DESC");
-//        ArrayList<String> sort = getByField(cards, "discount");
-        System.out.println(sort);
-    }
-
-    public static ArrayList<String> getByField(ArrayList<ItemCard> cards, String field ){
-        ArrayList<String> sortedBy = new ArrayList<>();
-        for (ItemCard card : cards) {
-            if (field.equals("title")) {
-                sortedBy.add(card.getTitle());
-            }
-            if (field.equals("description")) {
-                sortedBy.add(card.getDescription());
-            }
-            if (field.equals("price")) {
-                sortedBy.add(Double.toString(card.getPrice()));
-            }
-            if (field.equals("discount")) {
-                sortedBy.add(Double.toString(card.getDiscount()));
-            }
+        for( ItemCard item : items ) {
+            System.out.println("Title: " + item.title + ", Description: " + item.description + ", Price: " + item.price + ", Discount: " + item.discount );
         }
-        Collections.sort( sortedBy );
-        return sortedBy;
-    }
-    public static ArrayList<String> getByField(ArrayList<ItemCard> cards, String field, String sortBy ){
-        ArrayList<String> sortedBy = new ArrayList<>();
-        if( sortBy.equals( "ASC" ) ){
-            for (ItemCard card : cards) {
-                if (field.equals("title")) {
-                    sortedBy.add(card.getTitle());
-                }
-                if (field.equals("description")) {
-                    sortedBy.add(card.getDescription());
-                }
-                if (field.equals("price")) {
-                    sortedBy.add(Double.toString(card.getPrice()));
-                }
-                if (field.equals("discount")) {
-                    sortedBy.add(Double.toString(card.getDiscount()));
-                }
-            }
-            Collections.sort( sortedBy );
-        }
-        if( sortBy.equals( "DESC" ) ){
-            for (ItemCard card : cards) {
-                if (field.equals("title")) {
-                    sortedBy.add(card.getTitle());
-                }
-                if (field.equals("description")) {
-                    sortedBy.add(card.getDescription());
-                }
-                if (field.equals("price")) {
-                    sortedBy.add(Double.toString(card.getPrice()));
-                }
-                if (field.equals("discount")) {
-                    sortedBy.add(Double.toString(card.getDiscount()));
-                }
-            }
-            Collections.reverse( sortedBy );
-        }
-        return sortedBy;
     }
 }
